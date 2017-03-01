e946772663ebc9fabb91c070877c723acf7e4c34
lehn85 <lehn85@yahoo.com>
2017-03-01 16:52:25 +0300

    Insert, query db

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/sqlite3/index.d.ts" />
-/// <reference path="globals/uniq/index.d.ts" />
-/// <reference path="modules/body-parser/index.d.ts" />
-/// <reference path="modules/chart.js/index.d.ts" />
-/// <reference path="modules/express/index.d.ts" />
-/// <reference path="modules/pg/index.d.ts" />
