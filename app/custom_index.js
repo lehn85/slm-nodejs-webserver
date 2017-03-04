20deb47a1abc2b8f10faa57a9871b698b8eaf503
lehn85 <lehn85@yahoo.com>
2017-03-04 17:13:23 +0300

    use ng-repeat

--- app/custom_index.js
+++ app/custom_index.js
@@ -2,15 +2,18 @@
     var app = angular.module("app", ['chart.js', 'angular-dialgauge']);
 
     app.controller("chartController", function ($scope, $http) {
-
+        // functions
         $scope.getLastUpdateTime = getLastUpdateTime;
 
-        $scope.options = {
+        /////// Charts
+        var chartData = {
+            temp: {},
+            humid: {},
+            volt: {},
+            watt_per_m2: {},
+        };
+        var defaultChartOptions = {
             responsive: true,
-            title: {
-                display: true,
-                text: 'Solar panel'
-            },
             tooltips: {
                 mode: 'index',
                 intersect: false,
@@ -39,54 +42,154 @@
                 }]
             }
         };
-        $scope.series = {
-            volt1: ['Voltage'],
-            watt_per_m2: ['Watt per m2'],
-        };
-        $scope.colors = [{
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderWidth: 5,
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
-            spanGaps: false,
-        },
-        //{
-        //    backgroundColor: "rgba(75,192,192,0.4)",
-        //    borderWidth: 5,
-        //    borderColor: "rgba(255,0,0,1)",
-        //}
-        ];
 
-        $scope.data = {};
+        chartData.temp = {
+            title: "Temperature (*C)",
+            options: defaultChartOptions,
+            series: ["Temperature"],
+            colors: [{
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderWidth: 5,
+                borderColor: "rgba(255,0,0,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+                spanGaps: false,
+            }],
+            data: [],
+        };
+
+        chartData.humid = {
+            title: "Humidity (%)",
+            options: defaultChartOptions,
+            series: ["Humidity"],
+            colors: [{
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderWidth: 5,
+                borderColor: "rgba(255,0,0,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+                spanGaps: false,
+            }],
+            data: [],
+        };
+
+        chartData.volt = {
+            title: "Voltage (V)",
+            options: defaultChartOptions,
+            series: ["Voltage"],
+            colors: [{
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderWidth: 5,
+                borderColor: "rgba(75,192,192,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+                spanGaps: false,
+            }],
+            data: [],
+        };
+
+        chartData.watt_per_m2 = {
+            title: "Power per m2 (W/m2)",
+            options: defaultChartOptions,
+            series: ["Watt per m2"],
+            colors: [{
+                backgroundColor: "rgba(75,192,192,0.4)",
+                borderWidth: 5,
+                borderColor: "rgba(75,192,192,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                pointHoverBorderColor: "rgba(220,220,220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [65, 59, 80, 81, 56, 55, 40],
+                spanGaps: false,
+            }],
+            data: [],
+        };
+
+        $scope.arrayCharts = [chartData.temp, chartData.humid, chartData.volt, chartData.watt_per_m2];
 
         // get latest data (minute)
         $http.get("/data/latest/5000").then(
             function (response) {
                 console.info(response);
                 var list = response.data;
-                $scope.data.volt1 = [
+                // temp
+                chartData.temp.data = [
                     list.map(function (v, i, l) {
                         return {
                             x: moment.utc(Number.parseInt(v.time)), // postgreSQL return bigint as string
-                            y: v.volt1
+                            y: v.temp
                         };
                     }),
                 ];
 
-                $scope.data.watt_per_m2 = [
+                // humid                
+                chartData.humid.data = [
+                    list.map(function (v, i, l) {
+                        return {
+                            x: moment.utc(Number.parseInt(v.time)), // postgreSQL return bigint as string
+                            y: v.humid
+                        };
+                    }),
+                ];
+
+                // voltage data
+                chartData.volt.data = [
+                   list.map(function (v, i, l) {
+                       return {
+                           x: moment.utc(Number.parseInt(v.time)), // postgreSQL return bigint as string
+                           y: v.volt1
+                       };
+                   }),
+                ];
+
+                // watt per m2 data
+                chartData.watt_per_m2.data = [
                     list.map(function (v, i, l) {
                         return {
                             x: moment.utc(Number.parseInt(v.time)), // postgreSQL return bigint as string
@@ -101,12 +204,54 @@
                 console.error(exception);
             });
 
+        /////// Gauges
+        var gaugeData = {
+            temp: {
+                min: -40,
+                max: 50,
+                title: "Temperature",
+                units: "*C",
+                value: 0,
+            },
+            humid: {
+                min: 0,
+                max: 100,
+                title: "Humidity",
+                units: "%",
+                value: 0,
+            },
+            volt: {
+                min: 0,
+                max: 5,
+                title: "Voltage",
+                units: "V",
+                value: 0,
+            },
+            watt_per_m2: {
+                min: 0,
+                max: 100,
+                title: "Power per m2",
+                units: "W/m2",
+                value: 0,
+            },
+        };        
+
         // get very last data
         $http.get("/data/last").then(
             function (response) {
-                console.info(response);
-                $scope.lastData = response.data;
+                console.info(response);                
+                var data = response.data;
 
+                $scope.lastData = data;
+
+                if (!$scope.gaugeData)
+                    $scope.arrayGauges = [gaugeData.temp, gaugeData.humid, gaugeData.volt, gaugeData.watt_per_m2];
+
+                gaugeData.temp.value = data.temp;
+                gaugeData.humid.value = data.humid;
+                gaugeData.volt.value = data.volt1;
+                gaugeData.watt_per_m2.value = data.watt_per_m2;                
+                //$scope.$applyAsync();
             },
             function (exception) {
                 console.error(exception);
@@ -120,66 +265,3 @@
         }
     });
 })();
-
-//window.onload = function () {
-//    var ctx = document.getElementById("canvas").getContext("2d");
-//    window.myLine = new Chart(ctx, config);
-//};
-
-//document.getElementById('randomizeData').addEventListener('click', function () {
-//    config.data.datasets.forEach(function (dataset) {
-//        dataset.data = dataset.data.map(function () {
-//            return randomScalingFactor();
-//        });
-//    });
-
-//    window.myLine.update();
-//});
-
-var colorNames = Object.keys(window.chartColors);
-//document.getElementById('addDataset').addEventListener('click', function () {
-//    var colorName = colorNames[config.data.datasets.length % colorNames.length];
-//    var newColor = window.chartColors[colorName];
-//    var newDataset = {
-//        label: 'Dataset ' + config.data.datasets.length,
-//        backgroundColor: newColor,
-//        borderColor: newColor,
-//        data: [],
-//        fill: false
-//    };
-
-//    for (var index = 0; index < config.data.labels.length; ++index) {
-//        newDataset.data.push(randomScalingFactor());
-//    }
-
-//    config.data.datasets.push(newDataset);
-//    window.myLine.update();
-//});
-
-//document.getElementById('addData').addEventListener('click', function () {
-//    if (config.data.datasets.length > 0) {
-//        var month = MONTHS[config.data.labels.length % MONTHS.length];
-//        config.data.labels.push(month);
-
-//        config.data.datasets.forEach(function (dataset) {
-//            dataset.data.push(randomScalingFactor());
-//        });
-
-//        window.myLine.update();
-//    }
-//});
-
-//document.getElementById('removeDataset').addEventListener('click', function () {
-//    config.data.datasets.splice(0, 1);
-//    window.myLine.update();
-//});
-
-//document.getElementById('removeData').addEventListener('click', function () {
-//    config.data.labels.splice(-1, 1); // remove the label first
-
-//    config.data.datasets.forEach(function (dataset, datasetIndex) {
-//        dataset.data.pop();
-//    });
-
-//    window.myLine.update();
-//});
